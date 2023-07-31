564e666dac1f4da37892717d54a887e816db08a6
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-07-31 15:48:49 -0300

    create components, configure routing and create navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './accounts/accounts.component';
+import { AccountComponent } from './account/account.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { CardsComponent } from './cards/cards.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { LoansComponent } from './loans/loans.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/accounts', pathMatch: 'full' },
+  { path: 'accounts', component: AccountsComponent },
+  { path: 'accounts/:accountId', component: AccountComponent },
+  { path: 'transactions', component: TransactionsComponent },
+  { path: 'cards', component: CardsComponent },
+  { path: 'transfer', component: TransferComponent },
+  { path: 'loans', component: LoansComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { CardsComponent } from './cards/cards.component';
+import { AccountComponent } from './account/account.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { LoansComponent } from './loans/loans.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountsComponent,
+    CardsComponent,
+    AccountComponent,
+    TransactionsComponent,
+    TransferComponent,
+    LoansComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
