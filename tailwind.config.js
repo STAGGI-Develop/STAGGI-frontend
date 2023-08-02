d6c30cb51f2bac8ddbeda541f8948f4b923f40e0
Gastón Ríos <g.rios@outlook.com>
2023-08-02 09:38:58 -0300

    add sidebar style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: "rgba(0, 0, 0, 0.20)",
+        whitesmoke: "#ececec",
+        dimgray: "#4d4d4d",
+        steelblue: "#077cc0",
+        white: "#fff",
+      },
+      fontFamily: {
+        encode: "Encode Sans",
+        sora: "Sora",
+      },
+    },
+    fontSize: {
+      sm: "14px",
+    },
   },
   plugins: [],
 };
