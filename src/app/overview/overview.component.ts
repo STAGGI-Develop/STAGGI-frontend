6d207b27af030e4c4bf68c4906b390aa02f71d10
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-08-14 18:20:38 -0300

    feat (view): Add latest transactions to overview component

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Client } from '../interfaces';
+import { Client, Transaction } from '../interfaces';
 import { ClientsService } from '../clients.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-overview',
@@ -9,16 +10,33 @@ import { ClientsService } from '../clients.service';
 })
 export class OverviewComponent implements OnInit {
   client?: Client;
+  latestTransactions?: Transaction[];
 
-  constructor(private clientsService: ClientsService) {}
+  constructor(
+    private clientsService: ClientsService,
+    private accountsService: AccountsService
+  ) {}
 
   ngOnInit(): void {
     this.getClient();
+    this.getLatestTransactions();
   }
 
   getClient() {
-    this.clientsService
-      .getCurrentClient()
-      .subscribe((client) => (this.client = client));
+    this.clientsService.getCurrentClient().subscribe((client) => {
+      this.client = client;
+    });
+  }
+
+  getLatestTransactions() {
+    this.accountsService.getClientAccounts().subscribe((accounts) => {
+      const allTransactions = accounts.$values.flatMap(
+        (acc) => acc.transactions.$values
+      );
+
+      this.latestTransactions = allTransactions
+        .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+        .slice(0, 5);
+    });
   }
 }
